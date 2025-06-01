29d25608573d5c1818270f46189d1dcd5004ed2d
Alàa Chiba <alaa.chiba.pr@gmail.com>
2025-06-01 19:52:56 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,7 @@ import { MongooseModule } from '@nestjs/mongoose';
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(
-      process.env.MONGO_URI ||
-        'mongodb+srv://alaachibapr:JmcxOcV0EyVSNuBP@cluster0.kuwydf4.mongodb.net/nestTest?retryWrites=true&w=majority&appName=Cluster0',
+      process.env.MONGO_URI
     ),
     UsersModule,
   ],
